c5009412ab74fd1dc26d3ba52d72ed48ecb4928c
alex <alex.murphy@wowcher.co.uk>
2017-07-28 16:27:44 +0100

    changed step file name, changed cucumber version, modifed conf files

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -1,46 +1,32 @@
+//protractor.conf.js
 exports.config = {
+  // seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  getPageTimeout: 60000,
+  allScriptsTimeout: 500000,
+  framework: 'custom',
+  // path relative to the current config file
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  capabilities: {
+    'browserName': 'chrome',
+      proxy: {
+        proxyType: 'manual',
+        httpProxy: 'anl-iron-proxy:80',
+        sslProxy: 'anl-iron-proxy:80'
+      }
+  },
 
-	seleniumAddress: 'https://eu1.appium.testobject.com/wd/hub',
-	specs: ['specs/*spec.js'],
+  // Spec patterns are relative to this directory.
+  specs: [
+    'features/*.feature'
+  ],
 
-	// restartBrowserBetweenTests: true,
+  baseURL: 'http://localhost:8080/',
 
-	onPrepare: function() {
-		var caps = browser.getCapabilities();
-	},
-
-	framework: 'custom',
-	// path relative to the current config file
-	frameworkPath: require.resolve('protractor-cucumber-framework'),
-
-	multiCapabilities: [{
-		testobject_api_key: '2065298F3FB24F229F30BE75CDE8C989',
-		testobject_device: 'Google_Pixel_real',
-		testobject_test_name: "android - test name",
-		testobject_cache_device: "true",
-		browserName: 'chrome',
-		shardTestFiles: false,
-		maxInstances: 25
-	}
-	// add more configurations here
-	],
-
-	cucumberOpts: {
-	require: 'features/step_definitions/stepDefinitions.js',
-	tags: false,
-	format: 'pretty',
-	profile: false,
-	'no-source': true
-	},
-
-	onComplete: function(result) {
-		console.log("result is " + result);
-
-		var printSessionId = function(jobName) {
-			browser.getSession().then(function(session) {
-				console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
-			});
-		}
-		printSessionId("Insert Job Name Here");
-	}
+  cucumberOpts: {
+    require: 'features/step_definitions/stepDefinitions.js',
+    tags: false,
+    format: 'pretty',
+    profile: false,
+    'no-source': true
+  }
 };
---
+++ testobject_cucumber.conf.js
@@ -0,0 +1,50 @@
+exports.config = {
+
+	seleniumAddress: 'https://eu1.appium.testobject.com/wd/hub',
+	
+	// Spec patterns are relative to this directory.
+	specs: [
+	'features/*.feature'
+	],
+
+	// restartBrowserBetweenTests: true,
+
+	onPrepare: function() {
+		var caps = browser.getCapabilities();
+	},
+
+	framework: 'custom',
+	// path relative to the current config file
+	frameworkPath: require.resolve('protractor-cucumber-framework'),
+
+	multiCapabilities: [{
+		testobject_api_key: '2065298F3FB24F229F30BE75CDE8C989',
+		testobject_device: 'Google_Pixel_real',
+		testobject_test_name: "android - test name",
+		testobject_cache_device: "true",
+		browserName: 'chrome',
+		shardTestFiles: false,
+		maxInstances: 25
+	}
+	// add more configurations here
+	],
+
+	cucumberOpts: {
+	require: 'features/step_definitions/stepDefinitions.js',
+	tags: false,
+	format: 'pretty',
+	profile: false,
+	'no-source': true
+	},
+
+	onComplete: function(result) {
+		console.log("result is " + result);
+
+		var printSessionId = function(jobName) {
+			browser.getSession().then(function(session) {
+				console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+			});
+		}
+		printSessionId("Insert Job Name Here");
+	}
+};
