8d05a4334268713a76e149954e5c4a4b928c3d01
alex <alex.murphy@wowcher.co.uk>
2017-07-28 15:27:58 +0100

    added cucumber functionality to framework

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -9,6 +9,10 @@ exports.config = {
 		var caps = browser.getCapabilities();
 	},
 
+	framework: 'custom',
+	// path relative to the current config file
+	frameworkPath: require.resolve('protractor-cucumber-framework'),
+
 	multiCapabilities: [{
 		testobject_api_key: '2065298F3FB24F229F30BE75CDE8C989',
 		testobject_device: 'Google_Pixel_real',
@@ -20,6 +24,14 @@ exports.config = {
 	}
 	// add more configurations here
 	],
+
+	cucumberOpts: {
+	require: 'features/step_definitions/stepDefinitions.js',
+	tags: false,
+	format: 'pretty',
+	profile: false,
+	'no-source': true
+	},
 
 	onComplete: function(result) {
 		console.log("result is " + result);
